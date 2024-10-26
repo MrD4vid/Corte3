aa9837a4087deb74343bea3ec4d8a4c27b524f4a
DavidJ <dj_5463@hormail.com>
2024-10-26 17:51:29 -0500

    actualizacion1

---
+++ data.js
@@ -0,0 +1,13 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Camisa",
+        precio: 119.000,
+        categoria: "Camisas",
+        talla: ["S", "M", "L", "XL"],
+        color: ["Blanco"],
+        descripcion: "Camisa manga corta en tela tejida y largo regular, con molde amplio, botones contramarcados, perilla incluída, bolsillos en frente, almilla en posterior",
+        material: "Algodón",
+        stock: 50,
+        imagen: "./img/camisa1.jpg"
+    },
